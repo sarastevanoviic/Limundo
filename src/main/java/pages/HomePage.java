54101d0e6fd6b1804afa2d71d895e0c1cce76202
sarastevanoviic <160256699+sarastevanoviic@users.noreply.github.com>
2026-06-16 14:34:41 +0200

    changes

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,9 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import java.time.Duration;
+
 
 public class HomePage extends BasePage {
 
